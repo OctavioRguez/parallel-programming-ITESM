a51c4204defc7809ffe1b707f9505436ff498ae7
OctavioRguez <115122414+OctavioRguez@users.noreply.github.com>
2022-10-07 12:12:00 -0500

    Update pi_calculation.c

--- practice_1/pi_calculation.c
+++ practice_1/pi_calculation.c
@@ -2,45 +2,47 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS    5
+#include <unistd.h>
 
-int global_variable = 10;
+int global_pi = 0;
+pthread_mutex_t lock;
 
-void* calculate (){
-		
+void* calculate (void *arg){
+	pthread_mutex_lock(&lock);
+	
+	
+	
+	pthread_mutex_unlock(&lock);
 }
 
 
-void *PrintHello(void *threadid)
-{
-    long tid;
-    tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
+void *PrintHello(void *threadid){
+	long tid;
+	tid = (long)threadid;
+	printf("Hello World! It's me, thread #%ld!\n", tid);
 	global_variable++;
 	printf("Global variable %d\n",global_variable);
-
-    pthread_exit(NULL);
+	
+	pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[])
-{
-    pthread_t threads[NUM_THREADS];
+int main(int argc, char *argv[]){
+	pthread_t threads[argc];
+	
+	if (pthread_mutex_init(&lock, NULL) != 0){
+        	printf("\n mutex init failed\n");
+            	return 1;
+    	}
 
+	int rc;
+	long t;
+	for(t = 0;t < argc;t++){
+		rc = pthread_create(&threads[t], NULL, calculate, NULL);
+		if (rc){
+		    printf("ERROR; return code from pthread_create() is %d\n", rc);
+		    exit(-1);
+		}
+	}
 
-    int rc;
-    long t;
-
-    for(t=0;t<NUM_THREADS;t++){
-        printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-        if (rc){
-            printf("ERROR; return code from pthread_create() is %d\n", rc);
-            exit(-1);
-        }
-    }
-
-
-
-    /* Last thing that main() should do */
-    pthread_exit(NULL);
+	pthread_exit(NULL);
 }
