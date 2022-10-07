460397585cc7c4e2e593fe6457c1b8fa7709edce
OctavioRguez <115122414+OctavioRguez@users.noreply.github.com>
2022-10-07 12:37:46 -0500

    Update pi_calculation.c

--- practice_1/pi_calculation.c
+++ practice_1/pi_calculation.c
@@ -3,14 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <math.h>
 
-int global_pi = 0;
+long double global_pi = 0;
 pthread_mutex_t lock;
 
 void* calculate (void *arg){
 	pthread_mutex_lock(&lock);
 	
-	
+	npoints = 10000;
+	circle_count = 0;
+	for(j = 1; j < npoints; j++){
+		srand((unsigned int)time(NULL));
+		int n1 = rand() % 2;
+		srand((unsigned int)time(NULL));
+		int n2 = rand() % 2; 
+		
+		if (sqrt(pow(n1, 2) + pow(n2, 2)) < 1){
+			circle_count++;
+		}
+	}
+	global_pi += 4.0 * circle_count/npoints;
 	
 	pthread_mutex_unlock(&lock);
 }
@@ -43,6 +57,9 @@ int main(int argc, char *argv[]){
 		    exit(-1);
 		}
 	}
+	
+	pthread_mutex_destroy(&lock);
+	printf("%d\n", global_pi);
 
-	pthread_exit(NULL);
+	return 0;
 }
