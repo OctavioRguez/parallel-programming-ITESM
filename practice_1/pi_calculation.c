909a77926813e620fa954bf35f67f60019df0586
OctavioRguez <115122414+OctavioRguez@users.noreply.github.com>
2022-10-08 12:33:43 -0500

    Update pi_calculation.c

--- practice_1/pi_calculation.c
+++ practice_1/pi_calculation.c
@@ -24,8 +24,8 @@ void* calculate (void *arg){
 	pthread_mutex_lock(&lock);
 	
 	for(int j = 1; j < points_thread; j++){
-		double n1 = (double)rand_r(&seed)/2.0;
-		double n2 = (double)rand_r(&seed)/2.0;
+		double n1 = (double)rand_r(&seed)/(double)((unsigned)RAND_MAX + 1);
+		double n2 = (double)rand_r(&seed)/(double)((unsigned)RAND_MAX + 1);
 		
 		if (sqrt(pow(n1, 2) + pow(n2, 2)) <= 1){
 			circle_count++;
