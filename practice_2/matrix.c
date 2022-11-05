1b2db883a247827843111389bb0ddf4d15a9604f
OctavioRguez <115122414+OctavioRguez@users.noreply.github.com>
2022-11-05 13:34:28 -0600

    Update matrix.c

--- practice_2/matrix.c
+++ practice_2/matrix.c
@@ -27,6 +27,7 @@ int main(int argc, char** argv){
     threads_num = atoi(argv[1]);
   }
   omp_set_num_threads(threads_num);
+  printf("Number of threads: &d\n", threads_num);
 
   int i, j, k;
   srand(time(NULL));
@@ -34,19 +35,20 @@ int main(int argc, char** argv){
     for(j = 0; j < n; j++){
       a[i][j] = 1 + rand() % 101;
       b[i][j] = 1 + rand() % 101;
+      c[i][j] = 0;
     }
   }
 
 #pragma omp parallel for private(i, j, k) shared (a, b, c)
-  for(i = 0; i < n;i++){
-    for(j = 0; j < n; j++){
-      c[i][j] = 0;
-      for(k = 0; k < n; k++){
+  for(i = 0; i < n;++i){
+    for(j = 0; j < n; ++j){
+      for(k = 0; k < n; ++k){
         c[i][j] += (a[i][k] * b[k][j]);
       }
     }
   }
-
+ 
+  printf("Solution matrix:");
   for(i = 0; i < n; i++){
     for(j = 0; j < n; j++){
       printf("%d ", c[i][j]);
